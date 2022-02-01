1b7dff412f3ac46fd9315b6e5c4dbf6b2ff9d22d
pro-D-coder <2019pietcsdaksh49@poornima.org>
2022-02-01 22:59:22 +0530

    :construction: Changed Todo Table Name and Added Enum

--- common/abstract_model.py
+++ common/abstract_model.py
@@ -1,12 +1,13 @@
 from turtle import update
 from django.db import models
 from django.utils import timezone
+from uuid import uuid4
 
 class AbstractModel(models.Model):
     """
     An Abstract Model For All Models
     """
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
 
     createdAt = models.DateTimeField(default = timezone.now())
 
--- todo/models.py
+++ todo/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 from common.abstract_model import AbstractModel
-from common.status_enum import StatusType
-
 
 # Create your models here.
 class Todo(AbstractModel):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=60)
     description = models.CharField(max_length=200)
+
+    STATUS_TYPE = [
+        ('COMPLETED','COMPLETED'),
+        ('INCOMPLETE','INCOMPLETE'),
+    ]
     status = models.CharField(
         max_length=10,
-        default=StatusType.INCOMPLETE,
-        choices=[(tag,tag.value) for tag in StatusType]
+        default='INCOMPLETE',
+        choices=STATUS_TYPE,
         )
-
     def __str__(self) -> str:
         return self.title
+    
+    class Meta:
+        db_table = 'todo'
+    
+    
